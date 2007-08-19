04bf36550e69a12e2cfba4f4ce43daaa9e419a39
Dustin Sallings <dustin@spy.net>
2007-08-18 22:00:30 -0700

    Added related tags.

--- nextvista/app/video/views.py
+++ nextvista/app/video/views.py
@@ -12,15 +12,25 @@ def show_user(request, username):
 def show_tag(request, tag):
     tags=Tag.objects.filter(name__in=tag.split('+'))
     vres = Video.objects.filter(tags__in=tags)
+    related = {}
     videos=[]
     rv={}
     # Lame.  I need to figureo out how to get a proper intersection.
     for v in vres:
         has_all_tags = True
+        vtags = v.tags.all()
+        for t in vtags:
+            related[t.name] = t
         for t in tags:
-            if t not in v.tags.all():
+            if t not in vtags:
                 has_all_tags = False
         if has_all_tags:
             rv[v.id] = v
+        for t in tags:
+            if t.name in related:
+                del related[t.name]
     return render_to_response('video/tag.html',
-        {'tag': tag, 'tags', tags, 'videos': sorted(rv.values())})
+        {'tag': tag,
+        'tags': tags,
+        'related': sorted(related.values()),
+        'videos': sorted(rv.values())})
