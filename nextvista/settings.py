ae9d0968138d4c210e2991dc5588f3cb6bd7ea4d
Dustin Sallings <dustin@spy.net>
2007-09-23 22:16:54 -0700

    Removed some useless comments in the config

--- nextvista/settings.py
+++ nextvista/settings.py
@@ -67,8 +67,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'nextvista.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates".
-    # Always use forward slashes, even on Windows.
     os.path.expanduser("~/nextvista/templates"),
     os.path.expanduser("~/prog/nextvista/templates"),
 )
