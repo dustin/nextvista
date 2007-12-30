5ff639ec2652fdb7e72f42868ec611383250d195
Dustin Sallings <dustin@spy.net>
2007-12-29 17:14:22 -0800

    Some schema changes for the user model.

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users", :force => true do |t|
-      t.column :login,                     :string
-      t.column :email,                     :string
+      t.column :login,                     :string, :null => false, :limit => 40
+      t.column :email,                     :string, :null => false, :limit => 100
       t.column :crypted_password,          :string, :limit => 40
       t.column :salt,                      :string, :limit => 40
-      t.column :created_at,                :datetime
+      t.column :created_at,                :datetime, :null => false
       t.column :updated_at,                :datetime
       t.column :remember_token,            :string
       t.column :remember_token_expires_at, :datetime
