ee5c10a840dee9eb3a1fc7854ae10d4d495ca524
Dustin Sallings <dustin@spy.net>
2007-12-29 19:48:45 -0800

    Verify tagging on the videos.

--- test/unit/video_test.rb
+++ test/unit/video_test.rb
@@ -1,6 +1,8 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class VideoTest < Test::Unit::TestCase
+  include AuthenticatedTestHelper
+
   fixtures :videos, :tags, :languages, :users
 
   def test_video_sluggery
@@ -23,8 +25,11 @@ class VideoTest < Test::Unit::TestCase
     v=Video.new :submitter => users(:quentin), :submit_date => Time.now,
       :language => languages(:en), :duration => 152, :title => "This Is My Title",
       :descr => "Here's a description.", :long_descr => "Here's a long description."
-    v.tag_list = 'tag1 tag2 tag3'
-    v.save!
+    assert_difference Tag, :count do
+      v.tag_list = 'tag1, tag2, tag3'
+      v.save!
+    end
+    assert_equal %w(tag1 tag2 tag3), v.tag_list.sort
   end
 
 end
