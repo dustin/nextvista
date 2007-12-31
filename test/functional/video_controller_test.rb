b6fd889873b5ea3078d0342a5bf826d390c8ca19
Dustin Sallings <dustin@spy.net>
2007-12-30 19:49:55 -0800

    Added tests for video controller.

--- test/functional/video_controller_test.rb
+++ test/functional/video_controller_test.rb
@@ -5,14 +5,28 @@ require 'video_controller'
 class VideoController; def rescue_action(e) raise e end; end
 
 class VideoControllerTest < Test::Unit::TestCase
+
+  fixtures :videos, :users, :tags, :taggings
+
   def setup
     @controller = VideoController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index
+    get :index
+    assert_equal [1, 2], assigns['videos'].map(&:id).sort
   end
+
+  def test_by_slug
+    get :show, :slug => 'something-cool'
+    assert_equal videos(:one), assigns['video']
+  end
+
+  def test_by_slug2
+    get :show, :slug => 'something-less-cool'
+    assert_equal videos(:two), assigns['video']
+  end
+
 end
