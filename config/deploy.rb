48bf8273aad0b5820fb91513c1393d8d91bcc141
Dustin Sallings <dustin@spy.net>
2008-01-01 16:14:34 -0800

    Set up capistrano.

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, "nextvista"
+set :repository,  "http://hg.west.spy.net/hg/web/nextvista/"
+set :runner, 'www'
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/data/web/rails/#{application}"
+
+set :scm, :mercurial
+
+role :app, "basket.west.spy.net"
+role :web, "basket.west.spy.net"
+role :db,  "basket.west.spy.net", :primary => true
+
+desc "Starting happens via lighttpd.  Just need to stop here."
+deploy.task :start do
+  deploy.stop
+end
+desc "Starting happens via lighttpd.  Just need to stop here."
+deploy.task :restart do
+  deploy.stop
+end
+
